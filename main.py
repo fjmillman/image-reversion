dd21fcea1c29c4afea1b211bc5e9e82d436377b6
Freddie John Millman <freddie.millman@icloud.com>
2017-11-28 16:54:45 +0000

    Add load modules to script and change test program

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
-node1 = tf.constant(3.0, dtype=tf.float32)
-node2 = tf.constant(4.0)
-print(node1, node2)
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+
+print(sess.run(hello))
