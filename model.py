788c877e7d2759172d503195dd087c7ea3e3223f
Freddie John Millman <freddie.millman@icloud.com>
2018-03-21 19:09:44 +0000

    Alter strides

--- model.py
+++ model.py
@@ -123,7 +123,7 @@ class GAN(object):
 
         # encoder_1: [batch, 256, 256, in_channels] => [batch, 128, 128, ngf]
         with tf.variable_scope("encoder_1"):
-            output = gen_conv(generator_inputs, self.ngf, stride=2)
+            output = gen_conv(generator_inputs, self.ngf, stride=1)
             layers.append(output)
 
         layer_specs = [
@@ -139,9 +139,9 @@ class GAN(object):
         for encoder_layer, out_channels in enumerate(layer_specs):
             with tf.variable_scope(f"encoder_{len(layers) + 1}"):
                 rectified = lrelu(layers[-1], 0.2)
-                # stride = 1 if encoder_layer < 2 else 2
+                stride = 1 if encoder_layer < 2 else 2
                 # [batch, in_height, in_width, in_channels] => [batch, in_height / 2, in_width / 2, out_channels]
-                convolved = gen_conv(rectified, out_channels, stride=2)
+                convolved = gen_conv(rectified, out_channels, stride=stride)
                 output = batchnorm(convolved)
                 layers.append(output)
 
@@ -169,9 +169,9 @@ class GAN(object):
                     input = tf.concat([layers[-1], layers[skip_layer]], axis=3)
 
                 rectified = tf.nn.relu(input)
-                # stride = 1 if num_encoder_layers - decoder_layer < 4 else 2
+                stride = 1 if num_encoder_layers - decoder_layer < 4 else 2
                 # [batch, in_height, in_width, in_channels] => [batch, in_height * 2, in_width * 2, out_channels]
-                output = gen_deconv(rectified, out_channels, stride=2)
+                output = gen_deconv(rectified, out_channels, stride=stride)
                 output = batchnorm(output)
 
                 if dropout > 0.0:
@@ -183,7 +183,7 @@ class GAN(object):
         with tf.variable_scope("decoder_1"):
             input = tf.concat([layers[-1], layers[0]], axis=3)
             rectified = tf.nn.relu(input)
-            output = gen_deconv(rectified, generator_outputs_channels, stride=2)
+            output = gen_deconv(rectified, generator_outputs_channels, stride=1)
             output = tf.tanh(output)
             layers.append(output)
 
