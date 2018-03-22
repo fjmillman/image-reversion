21160c1ee54a9da2d6ad7e0c8705e6248214a845
Freddie John Millman <freddie.millman@icloud.com>
2018-03-22 13:12:12 +0000

    Remove strides completely and revert to u-net

--- model.py
+++ model.py
@@ -123,14 +123,14 @@ class GAN(object):
 
         # encoder_1: [batch, 256, 256, in_channels] => [batch, 128, 128, ngf]
         with tf.variable_scope("encoder_1"):
-            output = gen_conv(generator_inputs, self.ngf, stride=1)
+            output = gen_conv(generator_inputs, self.ngf)
             layers.append(output)
 
         layer_specs = [
-            self.ngf,  # encoder_2: [batch, 128, 128, ngf] => [batch, 64, 64, ngf * 2]
-            self.ngf,  # encoder_3: [batch, 64, 64, ngf * 2] => [batch, 32, 32, ngf * 4]
-            self.ngf * 2,  # encoder_4: [batch, 32, 32, ngf * 4] => [batch, 16, 16, ngf * 8]
-            self.ngf * 4,  # encoder_5: [batch, 16, 16, ngf * 8] => [batch, 8, 8, ngf * 8]
+            self.ngf * 2,  # encoder_2: [batch, 128, 128, ngf] => [batch, 64, 64, ngf * 2]
+            self.ngf * 4,  # encoder_3: [batch, 64, 64, ngf * 2] => [batch, 32, 32, ngf * 4]
+            self.ngf * 8,  # encoder_4: [batch, 32, 32, ngf * 4] => [batch, 16, 16, ngf * 8]
+            self.ngf * 8,  # encoder_5: [batch, 16, 16, ngf * 8] => [batch, 8, 8, ngf * 8]
             self.ngf * 8,  # encoder_6: [batch, 8, 8, ngf * 8] => [batch, 4, 4, ngf * 8]
             self.ngf * 8,  # encoder_7: [batch, 4, 4, ngf * 8] => [batch, 2, 2, ngf * 8]
             self.ngf * 8,  # encoder_8: [batch, 2, 2, ngf * 8] => [batch, 1, 1, ngf * 8]
@@ -139,9 +139,8 @@ class GAN(object):
         for encoder_layer, out_channels in enumerate(layer_specs):
             with tf.variable_scope(f"encoder_{len(layers) + 1}"):
                 rectified = lrelu(layers[-1], 0.2)
-                stride = 1 if encoder_layer < 2 else 2
                 # [batch, in_height, in_width, in_channels] => [batch, in_height / 2, in_width / 2, out_channels]
-                convolved = gen_conv(rectified, out_channels, stride=stride)
+                convolved = gen_conv(rectified, out_channels)
                 output = batchnorm(convolved)
                 layers.append(output)
 
@@ -149,11 +148,11 @@ class GAN(object):
 
         layer_specs = [
             (self.ngf * 8, 0.5),   # decoder_8: [batch, 1, 1, ngf * 8] => [batch, 2, 2, ngf * 8 * 2]
-            (self.ngf * 8, 0.0),   # decoder_7: [batch, 2, 2, ngf * 8 * 2] => [batch, 4, 4, ngf * 8 * 2]
-            (self.ngf * 4, 0.0),   # decoder_6: [batch, 4, 4, ngf * 8 * 2] => [batch, 8, 8, ngf * 8 * 2]
-            (self.ngf * 2, 0.0),   # decoder_5: [batch, 8, 8, ngf * 8 * 2] => [batch, 16, 16, ngf * 8 * 2]
-            (self.ngf, 0.0),   # decoder_4: [batch, 16, 16, ngf * 8 * 2] => [batch, 32, 32, ngf * 4 * 2]
-            (self.ngf, 0.0),   # decoder_3: [batch, 32, 32, ngf * 4 * 2] => [batch, 64, 64, ngf * 2 * 2]
+            (self.ngf * 8, 0.5),   # decoder_7: [batch, 2, 2, ngf * 8 * 2] => [batch, 4, 4, ngf * 8 * 2]
+            (self.ngf * 8, 0.5),   # decoder_6: [batch, 4, 4, ngf * 8 * 2] => [batch, 8, 8, ngf * 8 * 2]
+            (self.ngf * 8, 0.0),   # decoder_5: [batch, 8, 8, ngf * 8 * 2] => [batch, 16, 16, ngf * 8 * 2]
+            (self.ngf * 4, 0.0),   # decoder_4: [batch, 16, 16, ngf * 8 * 2] => [batch, 32, 32, ngf * 4 * 2]
+            (self.ngf * 2, 0.0),   # decoder_3: [batch, 32, 32, ngf * 4 * 2] => [batch, 64, 64, ngf * 2 * 2]
             (self.ngf, 0.0),       # decoder_2: [batch, 64, 64, ngf * 2 * 2] => [batch, 128, 128, ngf * 2]
         ]
 
@@ -169,9 +168,8 @@ class GAN(object):
                     input = tf.concat([layers[-1], layers[skip_layer]], axis=3)
 
                 rectified = tf.nn.relu(input)
-                stride = 1 if num_encoder_layers - decoder_layer < 4 else 2
                 # [batch, in_height, in_width, in_channels] => [batch, in_height * 2, in_width * 2, out_channels]
-                output = gen_deconv(rectified, out_channels, stride=stride)
+                output = gen_deconv(rectified, out_channels)
                 output = batchnorm(output)
 
                 if dropout > 0.0:
@@ -183,7 +181,7 @@ class GAN(object):
         with tf.variable_scope("decoder_1"):
             input = tf.concat([layers[-1], layers[0]], axis=3)
             rectified = tf.nn.relu(input)
-            output = gen_deconv(rectified, generator_outputs_channels, stride=1)
+            output = gen_deconv(rectified, generator_outputs_channels)
             output = tf.tanh(output)
             layers.append(output)
 
